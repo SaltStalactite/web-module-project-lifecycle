0cc2e3c91fdfc9f25ed06b98eb4beac6fd78b0e7
cc <votaryofturms@icloud.com>
2021-12-01 01:28:20 -0500

    set up state in app and componentDidMount

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 import React from 'react';
 import './App.css'
+import axios from 'axios';
 
 class App extends React.Component {
+  state = {
+    user: {},
+    followers: [],
+    input: 'SaltStalactite'
+  }
+
+  componentDidMount() {
+    axios.get('https://api.github.com/users/SaltStalactite')
+      .then(res => {
+        this.setState({
+          ...this.state,
+          user: res.data,
+        })
+      })
+      .catch(err => {
+        console.error(err)
+      });
+    axios.get('https://api.github.com/users/SaltStalactite/followers')
+      .then(res => {
+        this.setState({
+          ...this.state,
+          followers: res.data
+        })
+      })
+  }
+
   render() {
     return (<div className='App'>
-      Github Card
+      <h1>Github User Cards</h1>
     </div>);
   }
 }
